fa53de78d6771647ca0d236c27df0194be6bf93f
Marat Akhmadiev <m.akhma@yandex.ru>
2019-03-31 18:03:50 +0300

    creating

--- server/index.js
+++ server/index.js
@@ -6,17 +6,42 @@ let server = http.Server(app);
 
 let socketIO = require('socket.io');
 let io = socketIO(server);
+let users = [];
 
 const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
     console.log('user connected');
 
-    socket.on('new-message', (message) => {
-      console.log(message);
+    socket.on('join', function(data){
+      socket.join(data.room);      
+      console.log(data.user + ' joined the room : ' + data.room);
+
+      users.push("@" + data.user + " ");
+      users = users.filter( onlyUnique );
+
+      io.in(data.room).emit('new user joined', { user: data.user, users: users, message:'has joined this room.' });
+      // socket.broadcast.to(data.room).emit('new user joined', { user: data.user, users: users, message:'has joined this room.' });
+    });
+
+    socket.on('leave', function(data){
+    
+      console.log(data.user + 'left the room : ' + data.room);
+
+      socket.broadcast.to(data.room).emit('left room', {user:data.user, message:'has left this room.'});
+
+      socket.leave(data.room);
+    });
+
+    socket.on('message',function(data){
+      io.in(data.room).emit('new message', {user:data.user, userTo:data.userTo, message:data.message});
     });
 });
 
 server.listen(port, () => {
     console.log(`started on port: ${port}`);
 });
+
+function onlyUnique(value, index, self) { 
+  return self.indexOf(value) === index;
+}
